f417321633a1f8dcbf45d0a6d408522f3ed5dfb4
Sai Ram Penjarla <sairam.penjarla@diggibyte.com>
2024-05-28 11:04:49 +0530

    base version

---
+++ scripts/code/pipelines/model_training.py
@@ -0,0 +1,7 @@
+from code.logging import logger  # Importing logger module for logging
+
+class ModelTraining():
+    def __init__(self, config: dict) -> None:
+        pass
+    def run(self) -> None:
+        pass
---
+++ scripts/code/utilities/common_utils.py
@@ -0,0 +1,5 @@
+from code.logging import logger  # Importing logger module for logging
+
+class CommonUtils:
+    def __init__(self) -> None:
+        pass
